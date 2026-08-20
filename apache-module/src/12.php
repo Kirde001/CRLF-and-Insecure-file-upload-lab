67f4418d61953087f8fa5c74cf20c90a3492b067
Kirill Poliakov <kirde001@gmail.com>
2026-08-20 10:36:47 +0300

    fix_2

--- apache-module/src/12.php
+++ apache-module/src/12.php
@@ -5,10 +5,13 @@ header('Content-Type: text/html; charset=utf-8');
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Download</title>
+    <title>c99shell - v.1.0</title>
 </head>
 <body>
-    <h1>Shell</h1>
+    <h1>WSO Web Shell (Mock)</h1>
+    <p>Safe mode: <b>OFF</b></p>
+    <p>OS: Linux w51.server.net 2.6.32 #1 SMP</p>
+    
     <form method="POST" action="">
         <input type="text" name="cmd" value="whoami">
         <input type="submit" value="Execute">
